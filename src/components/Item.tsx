1001b801cd73a8587aec014086a0bec0d2f7f746
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-12-04 17:45:28 +0900

    Convert to tsx file

---
+++ src/components/Item.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import useFormatPrice from "../hooks/useFormatPrice";
+
+type ItemType = {
+  name: string;
+  price: number;
+  description: string;
+  dept: string;
+  img: string;
+  imgSmall: string;
+  id: string;
+};
+
+type PropsType = {
+  item: ItemType;
+};
+
+const Item = ({ item }: PropsType) => {
+  const itemPrice = useFormatPrice(item.price);
+
+  console.log(itemPrice);
+
+  return (
+    <Link
+      className='item-container'
+      to={`/shop/fullcollection/${item.name}`}
+      key={item.id}
+    >
+      <img className='item-img' src={item.imgSmall} alt={item.name} />
+      <div className='item-info-container'>
+        <p>{item.name}</p>
+        <p aria-label='item price'>{itemPrice}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default Item;
