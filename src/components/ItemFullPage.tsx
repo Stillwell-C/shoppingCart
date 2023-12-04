d461126a2a981377d5382bb63ff88aba53770263
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-12-04 18:11:05 +0900

    Convert to work with new data

--- src/components/ItemFullPage.tsx
+++ src/components/ItemFullPage.tsx
@@ -1,65 +1,49 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import Footer from "./Footer";
-import NotFound from "./NotFound";
+import fullItemList from "../Data/FullItemList";
+import useFormatPrice from "../hooks/useFormatPrice";
+import useCartContext from "../hooks/useCartContext";
 
-const ItemFullPage = ({ shoppingCart, setShoppingCart, itemList }) => {
-  const { itemName } = useParams();
-  const [currentItem] = itemList.filter((item) => item.name === itemName);
-  let displayItem = true;
-  if (!currentItem) {
-    displayItem = false;
-  }
+const ItemFullPage = () => {
+  const { itemID } = useParams();
+  const navigate = useNavigate();
+  const { REDUCER_ACTIONS, dispatch } = useCartContext();
+
+  const [currentItem] = fullItemList.filter(
+    (item) => item.id === `item${itemID}`
+  );
+
+  const itemPrice = useFormatPrice(currentItem?.price);
+
+  useEffect(() => {
+    if (!currentItem) {
+      navigate("/notfound");
+    }
+  }, []);
 
   const handleAddToCart = () => {
-    const currentShoppingCart = [...shoppingCart];
-    let updatedShoppingCart;
-    let matches = currentShoppingCart.filter(
-      (item) => item.name === currentItem.name
-    );
-    if (matches.length > 0) {
-      const itemIndex = currentShoppingCart.findIndex(
-        (obj) => obj.name === currentItem.name
-      );
-      updatedShoppingCart = [...currentShoppingCart];
-      updatedShoppingCart[itemIndex].quantity += 1;
-      setShoppingCart(updatedShoppingCart);
-      return;
-    }
-
-    updatedShoppingCart = [
-      ...currentShoppingCart,
-      {
-        name: currentItem.name,
-        price: currentItem.price,
-        quantity: 1,
-        serial: currentItem.serial,
-        imgSmall: currentItem.imgSmall,
-      },
-    ];
-    setShoppingCart(updatedShoppingCart);
+    dispatch({
+      type: REDUCER_ACTIONS.ADD,
+      payload: { ...currentItem, qty: 1 },
+    });
   };
 
   return (
-    <>
-      {displayItem && (
-        <div className='item-full-page'>
-          <div className='item-full-container'>
-            <h1 className='item-full-title'>{currentItem.name}</h1>
-            <img
-              className='item-img-medium'
-              src={currentItem.img}
-              alt={currentItem.name}
-            />
-            <p>{currentItem.description}</p>
-            <p>L {currentItem.price}</p>
-            <button onClick={handleAddToCart}>Add to cart</button>
-          </div>
-          <Footer />
-        </div>
-      )}
-      {!displayItem && <NotFound />}
-    </>
+    <div className='item-full-page'>
+      <div className='item-full-container'>
+        <h1 className='item-full-title'>{currentItem?.name}</h1>
+        <img
+          className='item-img-medium'
+          src={currentItem?.img}
+          alt={currentItem?.name}
+        />
+        <p>{currentItem?.description}</p>
+        <p>{itemPrice}</p>
+        <button onClick={handleAddToCart}>Add to cart</button>
+      </div>
+      <Footer />
+    </div>
   );
 };
 
