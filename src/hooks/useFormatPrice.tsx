d47e5b13ed789079e46f22458e6b5e17a0cc587f
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-12-04 18:12:12 +0900

    Initial commit

---
+++ src/hooks/useFormatPrice.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type PropsType = {
+  price: number;
+};
+
+const useFormatPrice = (price: number): string => {
+  return new Intl.NumberFormat("ro-MD", {
+    style: "currency",
+    currency: "MDL",
+  }).format(price);
+};
+
+export default useFormatPrice;
