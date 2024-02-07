12ccdac0d07e6d60452b8975b3be415a316955a3
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-07 10:13:22 +0900

    Convert to tsx

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -24,7 +24,7 @@ const Checkout = () => {
     console.log(formState);
   }, [formState]);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const validation = validateForm(formState);
     if (!validation) return;
