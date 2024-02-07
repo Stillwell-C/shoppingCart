7fa3bb9ed91c8a3364cb85d5012a06798c504b87
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-07 12:43:59 +0900

    Add loading spinner

--- src/components/OrderConfirmation.tsx
+++ src/components/OrderConfirmation.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { BarLoader } from "react-spinners";
 
 const OrderConfirmation = () => {
   const navigate = useNavigate();
@@ -14,6 +15,7 @@ const OrderConfirmation = () => {
     <div className='redirect-div'>
       <h1>Success!</h1>
       <h2>Your order has been recieved.</h2>
+      <BarLoader height={6} />
     </div>
   );
 };
