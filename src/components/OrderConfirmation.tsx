8a79055b676ed3d5d8485fafc16af5e5a5c4a3da
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-13 11:40:11 +0900

    Display order number

--- src/components/OrderConfirmation.tsx
+++ src/components/OrderConfirmation.tsx
@@ -1,19 +1,15 @@
 import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const OrderConfirmation = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const timer = setTimeout(() => navigate("/"), 2500);
-
-    return () => clearTimeout(timer);
-  }, []);
+  const location = useLocation();
 
   return (
     <div className='redirect-div'>
-      <h1>Success!</h1>
+      <h2>Success!</h2>
       <h2>Your order has been recieved.</h2>
+      <p>Order No: {location?.state?.orderNumber}</p>
+      <Link to='/'>Return Home</Link>
     </div>
   );
 };
