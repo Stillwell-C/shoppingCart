c0a257c7afb5195b434f1d88cb505e3e8d9659ee
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-15 13:47:08 +0900

    Move email form to own component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,31 +1,7 @@
-import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
-const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+import EmailForm from "./EmailForm";
 
 const Footer = () => {
-  const [formMessage, setFormMessage] = useState("none");
-  const [email, setEmail] = useState("");
-
-  useEffect(() => {
-    setFormMessage("none");
-  }, []);
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const valid = emailRegex.test(email);
-    if (valid) {
-      setFormMessage("success");
-      setEmail("");
-      return;
-    }
-    setFormMessage("failure");
-  };
-
-  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  };
-
   return (
     <footer>
       <div className='footer-container'>
@@ -36,27 +12,7 @@ const Footer = () => {
               Get information on discounts and sales delivered straight to your
               inbox
             </p>
-            <form noValidate onSubmit={handleSubmit}>
-              <input
-                type='email'
-                value={email}
-                onChange={handleInput}
-                placeholder='Enter your email address'
-                autoComplete='false'
-                aria-label='Sign up for updates'
-              />
-              <button type='submit'>Sign Up</button>
-            </form>
-            {formMessage === "success" && (
-              <span className='footer-form-msg footer-form-success'>
-                Thank you for signing up!
-              </span>
-            )}
-            {formMessage === "failure" && (
-              <span className='footer-form-msg footer-form-failure'>
-                Please input a valid email address.
-              </span>
-            )}
+            <EmailForm />
           </div>
           <div>
             <h1>in hands</h1>
