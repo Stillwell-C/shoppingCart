9fc4acf42773d1aaaf474920d2fb72a5f0e0718c
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-16 17:09:35 +0900

    Add tailwind classes

--- src/components/CartDropDown.tsx
+++ src/components/CartDropDown.tsx
@@ -1,6 +1,8 @@
 import useCartContext from "../hooks/useCartContext";
 import { Link } from "react-router-dom";
 import CartDropDownItem from "./CartDropDownItem";
+import FocusTrap from "focus-trap-react";
+import { useEffect } from "react";
 
 type PropsType = {
   setCartOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -9,12 +11,31 @@ type PropsType = {
 const CartDropDown = ({ setCartOpen }: PropsType) => {
   const { cart, itemTotal, priceTotal } = useCartContext();
 
+  useEffect(() => {
+    //This will close modal with escape key
+    function keyListener(e: KeyboardEvent) {
+      if (e.code === "Escape") {
+        setCartOpen(false);
+      }
+    }
+
+    document.addEventListener("keydown", keyListener);
+
+    return () => document.removeEventListener("keydown", keyListener);
+  }, []);
+
   return (
     <>
-      <div className='shopping-cart-dropdown'>
-        <div className='shopping-dropdown-content'>
-          <div className='dropdown-header'>
-            <h2>Shopping Cart</h2>
+      <div className='absolute w-72 top-24 right-0 bottom-3 z-20 '>
+        <div
+          role='dialog'
+          aria-label='shopping cart dialog'
+          className=' px-4 min-h-80 max-h-[calc(90vh-96px)] w-64 bg-white border border-solid border-[#f4f4f4] rounded overflow-y-auto'
+        >
+          <div className='flex justify-between items-center m-4 '>
+            <h2 id='shopping-cart-heading' className='text-xl font-semibold'>
+              Shopping Cart
+            </h2>
             <button
               onClick={() => setCartOpen(false)}
               aria-label='close shopping cart menu'
@@ -23,14 +44,19 @@ const CartDropDown = ({ setCartOpen }: PropsType) => {
             </button>
           </div>
           {itemTotal < 1 && (
-            <div className='dropdown-empty-container'>
+            <div className='flex flex-col items-center justify-center gap-1 my-8'>
               <p>Your cart is empty.</p>
               <Link to='/shop'>
-                <button onClick={() => setCartOpen(false)}>Shop Now</button>
+                <button
+                  className='grey-button'
+                  onClick={() => setCartOpen(false)}
+                >
+                  Shop Now
+                </button>
               </Link>
             </div>
           )}
-          <ul className='dropdown-items'>
+          <ul className='flex flex-col gap-4 list-none'>
             {cart.map((item) => (
               <CartDropDownItem
                 item={item}
@@ -39,9 +65,11 @@ const CartDropDown = ({ setCartOpen }: PropsType) => {
               />
             ))}
           </ul>
-          <div className='dropdown-footer'>
-            <h3 aria-label='total price'>Total: {priceTotal}</h3>
-            <div className='dropdown-footer-buttons'>
+          <div className='flex flex-col my-5'>
+            <h3 aria-label='total price' className='font-semibold'>
+              Total: {priceTotal}
+            </h3>
+            <div className='flex flex-col gap-2 mt-4'>
               {itemTotal < 1 && (
                 <div>
                   <button disabled>Checkout Now</button>
@@ -49,20 +77,28 @@ const CartDropDown = ({ setCartOpen }: PropsType) => {
               )}
               {itemTotal >= 1 && (
                 <Link to='/checkout'>
-                  <button onClick={() => setCartOpen(false)}>
+                  <button
+                    className='grey-button'
+                    onClick={() => setCartOpen(false)}
+                  >
                     Checkout Now
                   </button>
                 </Link>
               )}
               <Link to='/cart'>
-                <button onClick={() => setCartOpen(false)}>View Cart</button>
+                <button
+                  className='grey-button'
+                  onClick={() => setCartOpen(false)}
+                >
+                  View Cart
+                </button>
               </Link>
             </div>
           </div>
         </div>
       </div>
       <div
-        className='shopping-dropdown-overlay'
+        className='fixed top-24 right-0 left-0 bottom-0 z-10'
         onClick={() => setCartOpen(false)}
       ></div>
     </>
