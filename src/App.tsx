669965623909901bc1f94d38b85360ac5f2cd848
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-16 12:24:08 +0900

    Add layout

--- src/App.tsx
+++ src/App.tsx
@@ -11,35 +11,33 @@ import NotFound from "./components/NotFound";
 import CartFullPage from "./components/CartFullPage";
 import Orders from "./components/Orders";
 import OrderCancelConfirmation from "./components/OrderCancelConfirmation";
+import Layout from "./components/Layout";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <div className='body-container'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/shop'>
-            <Route index element={<Shop />} />
-            <Route path=':collectionName' element={<Shop />} />
-            <Route path='fullcollection/:itemID' element={<ItemFullPage />} />
-          </Route>
-          <Route path='/cart' element={<CartFullPage />} />
-          <Route path='/checkout' element={<Checkout />} />
-          <Route path='/orderconfirmation' element={<OrderConfirmation />} />
-          <Route path='/orders'>
-            <Route index element={<Orders />} />
-            <Route
-              path='cancelconfirmation'
-              element={<OrderCancelConfirmation />}
-            />
-          </Route>
-          <Route path='/notfound' element={<NotFound />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </div>
-    </div>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/shop'>
+          <Route index element={<Shop />} />
+          <Route path=':collectionName' element={<Shop />} />
+          <Route path='fullcollection/:itemID' element={<ItemFullPage />} />
+        </Route>
+        <Route path='/cart' element={<CartFullPage />} />
+        <Route path='/checkout' element={<Checkout />} />
+        <Route path='/orderconfirmation' element={<OrderConfirmation />} />
+        <Route path='/orders'>
+          <Route index element={<Orders />} />
+          <Route
+            path='cancelconfirmation'
+            element={<OrderCancelConfirmation />}
+          />
+        </Route>
+        <Route path='/notfound' element={<NotFound />} />
+        <Route path='*' element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
