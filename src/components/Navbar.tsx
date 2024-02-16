52d6d96995c65c706757cfae672ca1dd95297fd6
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-16 17:10:15 +0900

    Add tailwind classes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
-import { NavLink, Link } from "react-router-dom";
+import { NavLink, Link, useLocation } from "react-router-dom";
 import shoppingbag from "../assets/shopping-cart.svg";
 import useCartContext from "../hooks/useCartContext";
 import CartDropDown from "./CartDropDown";
 
 const Navbar = () => {
+  const { pathname } = useLocation();
+
+  const shopActive = pathname.match(/^\/shop/i);
+  const aboutActive = pathname.match(/^\/about/i);
+
   const [cartOpen, setCartOpen] = useState(false);
   const { itemTotal } = useCartContext();
 
@@ -12,28 +17,52 @@ const Navbar = () => {
     setCartOpen((prev) => !prev);
   };
 
+  const handleClose = () => {
+    setCartOpen(false);
+  };
+
   return (
-    <header>
-      <div className='nav-container'>
-        <Link to='/' className='nav-title' aria-label='link to home page'>
-          <h1 className='nav-title-top'>in hands</h1>
-          <h4 className='nav-title-bottom'>lifestyle & boutique</h4>
-        </Link>
-        <nav className='nav-right'>
-          <NavLink to='/about' aria-label='link to about page'>
+    <header className=' h-24 p-6 z-10 fixed w-full top-0 left-0 bg-white border-solid border-b border-[#f1f1f1] '>
+      <div className='w-full h-full flex justify-between content-center '>
+        <div className='h-full flex justify-center content-center'>
+          <Link
+            to='/'
+            className='h-full flex flex-col justify-center content-center transition-all ease-in-out hover:scale-[1.01]'
+            aria-label='move to home page'
+            onClick={handleClose}
+          >
+            <h1 className='font-light text-5xl tracking-[.5rem] underline'>
+              in hands
+            </h1>
+            <p className='text-base'>lifestyle & boutique</p>
+          </Link>
+        </div>
+        <nav className='flex items-center gap-4'>
+          <NavLink
+            className={`text-xl ${aboutActive ? "font-bold" : ""}`}
+            to='/about'
+            aria-label='move to about page'
+            onClick={handleClose}
+          >
             About
           </NavLink>
-          <NavLink to='/shop' aria-label='link to shop page'>
+          <NavLink
+            className={`text-xl ${shopActive ? "font-bold" : ""}`}
+            to='/shop'
+            aria-label='move to shop page'
+            onClick={handleClose}
+          >
             Shop
           </NavLink>
-          <div className='shoppingbag-container'>
-            <span data-testid='shoppingbag-quantity'>{itemTotal}</span>
+          <div className='flex items-center gap-1'>
+            <span aria-label='number of items in cart'>{itemTotal}</span>
             <button
               onClick={handleShoppingBag}
               aria-label='open shopping cart menu'
+              className='bg-transparent cursor-pointer'
             >
               <img
-                className='shoppingbag-logo'
+                className=' h-7 '
                 src={shoppingbag}
                 alt='shopping bag button'
               />
