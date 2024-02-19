a9338e7c14541fa50f8c3979609e7d06920068f5
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-19 15:54:35 +0900

    Add tailwind classes

--- src/components/SkeletonItem.tsx
+++ src/components/SkeletonItem.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const SkeletonItem = () => {
   return (
-    <div className='item-container'>
-      <div className='item-img skeleton-background'></div>
-      <div className='item-info-container'>
-        <div className='skeleton-text-line skeleton-background'></div>
-        <div className='skeleton-text-line skeleton-background'></div>
+    <div className='w-72 h-[450px] shadow-lg rounded-lg flex flex-col justify-center items-center transition-all ease-in-out hover:scale-[1.01] animate-pulse'>
+      <div className='h-[300px] w-[210px] object-cover rounded bg-gray-300'></div>
+      <div className='w-[200px] mt-3'>
+        <div className='h-4 w-20 bg-gray-300 mb-1'></div>
+        <div className='h-4 w-32 bg-gray-300'></div>
       </div>
     </div>
   );
