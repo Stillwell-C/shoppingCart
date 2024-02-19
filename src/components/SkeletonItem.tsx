98a0e3c9116ea032ec69e4a11ce87314ba9b0744
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-19 16:10:24 +0900

    Update tailwind classes

--- src/components/SkeletonItem.tsx
+++ src/components/SkeletonItem.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const SkeletonItem = () => {
   return (
     <div className='w-72 h-[450px] shadow-lg rounded-lg flex flex-col justify-center items-center transition-all ease-in-out hover:scale-[1.01] animate-pulse'>
-      <div className='h-[300px] w-[210px] object-cover rounded bg-gray-300'></div>
+      <div className='h-[300px] w-[210px] rounded bg-gray-300'></div>
       <div className='w-[200px] mt-3'>
         <div className='h-4 w-20 bg-gray-300 mb-1'></div>
         <div className='h-4 w-32 bg-gray-300'></div>
