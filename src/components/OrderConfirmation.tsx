ceb54900dc8263b58a48ea014b327f636f9327ff
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-19 16:18:10 +0900

    Add tailwind classes

--- src/components/OrderConfirmation.tsx
+++ src/components/OrderConfirmation.tsx
@@ -1,14 +1,15 @@
-import React, { useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 const OrderConfirmation = () => {
   const location = useLocation();
 
   return (
-    <div className='redirect-div'>
-      <h2>Success!</h2>
-      <h2>Your order has been recieved.</h2>
-      <p>Order No: {location?.state?.orderNumber}</p>
+    <div className='mt-24 w-full flex flex-col items-center gap-3'>
+      <div className='flex flex-col w-full items-center'>
+        <h2 className='mt-12 text-4xl mb-1'>Success!</h2>
+        <p className='text-2xl'>Your order has been recieved.</p>
+      </div>
+      <p className='text-lg'>Order No: {location?.state?.orderNumber}</p>
       <Link to='/'>Return Home</Link>
     </div>
   );
