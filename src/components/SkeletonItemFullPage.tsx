d32c5677719c7116c096f5c1d100529508b0fd66
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-19 16:33:43 +0900

    Change heading size

--- src/components/SkeletonItemFullPage.tsx
+++ src/components/SkeletonItemFullPage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const SkeletonItemFullPage = () => {
   return (
     <div className='mt-32 mb-32 mx-auto min-h-screen flex flex-col animate-pulse'>
-      <div className='h-6 w-24 mb-8 bg-gray-300 self-center'></div>
+      <div className='h-10 w-32 mb-8 bg-gray-300 self-center'></div>
       <div className='cover w-[350px] h-[500px] rounded bg-gray-300'></div>
       <div className='flex flex-col mt-4 mb-2 gap-1'>
         <div className='h-4 w-36 bg-gray-300 mb-1'></div>
