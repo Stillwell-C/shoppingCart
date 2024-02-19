bc579143ad508761a3525467e56c542f26227f3c
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-19 16:10:46 +0900

    Add tailwind classes

--- src/components/SkeletonItemFullPage.tsx
+++ src/components/SkeletonItemFullPage.tsx
@@ -2,15 +2,17 @@ import React from "react";
 
 const SkeletonItemFullPage = () => {
   return (
-    <div className='item-full-container'>
-      <div className='item-full-title skeleton-item-title skeleton-background'></div>
-      <div className='item-img-medium skeleton-background skeleton-item-img'></div>
-      <div className='skeleton-item-information'>
-        <div className='skeleton-text-line skeleton-background'></div>
-        <div className='skeleton-text-line skeleton-background'></div>
+    <div className='mt-32 mb-32 mx-auto min-h-screen flex flex-col animate-pulse'>
+      <div className='h-6 w-24 mb-8 bg-gray-300 self-center'></div>
+      <div className='cover w-[350px] h-[500px] rounded bg-gray-300'></div>
+      <div className='flex flex-col mt-4 mb-2 gap-1'>
+        <div className='h-4 w-36 bg-gray-300 mb-1'></div>
+        <div className='h-4 w-28 bg-gray-300'></div>
       </div>
       <div>
-        <button disabled={true}>Add to cart</button>
+        <button className='grey-button py-2 px-3' disabled={true}>
+          Add to cart
+        </button>
       </div>
     </div>
   );
