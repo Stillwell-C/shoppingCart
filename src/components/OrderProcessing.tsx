3f8e3aa5bdc355a29319d1ea7ee5ebf7763a8c25
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-20 11:38:50 +0900

    Add tailwind classes

--- src/components/OrderProcessing.tsx
+++ src/components/OrderProcessing.tsx
@@ -3,10 +3,10 @@ import { BarLoader } from "react-spinners";
 
 const OrderProcessing = () => {
   return (
-    <div>
-      <h2>Order Processing</h2>
-      <p>Do not refresh page or close window.</p>
-      <BarLoader height={6} />
+    <div className='mt-24 mb-32 mx-auto w-4/5 xl:w-[1024px] min-h-screen flex flex-col items-center'>
+      <h2 className='mt-10 text-3xl'>Order Processing</h2>
+      <p className='text-xl mb-2'>Do not refresh page or close window.</p>
+      <BarLoader height={6} width={150} />
     </div>
   );
 };
