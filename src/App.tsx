f64a4d79ed838fe088bcd397bb7d5af3af7754c8
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-20 11:47:43 +0900

    Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Shop from "./components/Shop";
