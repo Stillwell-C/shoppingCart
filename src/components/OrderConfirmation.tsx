487d3845f58fad8f2ca20174dac8e280459680a5
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-20 11:39:36 +0900

    Add tailwind classes

--- src/components/OrderConfirmation.tsx
+++ src/components/OrderConfirmation.tsx
@@ -6,11 +6,13 @@ const OrderConfirmation = () => {
   return (
     <div className='mt-24 w-full flex flex-col items-center gap-3'>
       <div className='flex flex-col w-full items-center'>
-        <h2 className='mt-12 text-4xl mb-1'>Success!</h2>
+        <h2 className='mt-12 text-4xl'>Success!</h2>
         <p className='text-2xl'>Your order has been recieved.</p>
       </div>
       <p className='text-lg'>Order No: {location?.state?.orderNumber}</p>
-      <Link to='/'>Return Home</Link>
+      <Link className='text-lg underline' to='/'>
+        Return Home
+      </Link>
     </div>
   );
 };
