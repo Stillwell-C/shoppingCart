2422b2ee78b000b93290b5ac0905f9e7b4bede55
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-28 19:19:19 +0900

    Adjust breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,10 @@ module.exports = {
     },
     screens: {
       smallMobile: "350px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
     },
   },
   plugins: [],
