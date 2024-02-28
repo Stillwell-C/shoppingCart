abc2cb3f228eec3d7cf000c379ddf1936aa8e3c8
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-28 19:05:46 +0900

    Add responsive widths

--- src/components/CreditCard.tsx
+++ src/components/CreditCard.tsx
@@ -72,139 +72,141 @@ const CreditCard = ({ dispatch, formState }: PropsType) => {
 
   return (
     <div>
-      <div className='flex items-center justify-start gap-4 w-[650px]'>
-        <div className='relative h-[50px] w-[650px] overflow-hidden text-lg checkout-input-div'>
-          <input
-            type='text'
-            name='creditCardNumber'
-            autoComplete='off'
-            value={spaceCCNum(formState.creditCardNumber.value)}
-            onChange={(e) => handleCreditCardNum(e.target.value)}
-            onFocus={() => setCardNumberFocus(true)}
-            onBlur={() => setCardNumberFocus(false)}
-            maxLength={19}
-            aria-invalid={formState.creditCardNumber.hasError}
-            aria-describedby='creditcard-number-note'
-            aria-label='credit card number'
-            className='peer w-full h-full pt-5 text-[#333] outline-none'
-            required
-          />
-          <label
-            className='absolute bottom-0 left-[0%] w-full h-full pointer-events-none border-b border-solid border-black after:content-[" "] after:absolute after:h-full after:w-full after:left-0 -after:bottom-[1px] after:border-b-[2px] after:border-solid after:border-black after:-translate-x-[105%] after:transition-all after:duration-300 after:ease peer-focus:after:translate-x-[0%] peer-valid:after:translate-x-[0%]'
-            htmlFor='creditCardNumber'
-          >
-            <span
-              className={`absolute bottom-1 left-0 transition-all ease duration-500 ${
-                formState.creditCardNumber.value.length || cardNumberFocus
-                  ? "-translate-y-[130%] text-sm"
-                  : " "
-              }`}
+      <div className='flex flex-col sm:flex-row justify-start items-start gap-4 md:w-[650px]'>
+        <div className='md:w-[650px]'>
+          <div className='relative h-[50px] overflow-hidden text-lg checkout-input-div'>
+            <input
+              type='text'
+              name='creditCardNumber'
+              autoComplete='off'
+              value={spaceCCNum(formState.creditCardNumber.value)}
+              onChange={(e) => handleCreditCardNum(e.target.value)}
+              onFocus={() => setCardNumberFocus(true)}
+              onBlur={() => setCardNumberFocus(false)}
+              maxLength={19}
+              aria-invalid={formState.creditCardNumber.hasError}
+              aria-describedby='creditcard-number-note'
+              aria-label='credit card number'
+              className='peer w-full h-full pt-5 text-[#333] outline-none'
+              required
+            />
+            <label
+              className='absolute bottom-0 left-[0%] w-full h-full pointer-events-none border-b border-solid border-black after:content-[" "] after:absolute after:h-full after:w-full after:left-0 -after:bottom-[1px] after:border-b-[2px] after:border-solid after:border-black after:-translate-x-[105%] after:transition-all after:duration-300 after:ease peer-focus:after:translate-x-[0%] peer-valid:after:translate-x-[0%]'
+              htmlFor='creditCardNumber'
             >
-              Credit / Debit Card Number*
-            </span>
-          </label>
+              <span
+                className={`absolute bottom-1 left-0 transition-all ease duration-500 ${
+                  formState.creditCardNumber.value.length || cardNumberFocus
+                    ? "-translate-y-[130%] text-sm"
+                    : " "
+                }`}
+              >
+                Credit / Debit Card Number*
+              </span>
+            </label>
+          </div>
+          <span
+            id='creditcard-number-note'
+            className={
+              formState.creditCardNumber.hasError
+                ? "text-rose-600 font-semibold text-lg"
+                : "absolute -left-[9999px]"
+            }
+            ref={numberErrorRef}
+          >
+            {formState.creditCardNumber.error}
+          </span>
         </div>
       </div>
-      <div className='text-rose-600 font-semibold text-lg w-[650px]'>
-        <span
-          id='creditcard-number-note'
-          className={
-            formState.creditCardNumber.hasError ? "" : "absolute -left-[9999px]"
-          }
-          ref={numberErrorRef}
-        >
-          {formState.creditCardNumber.error}
-        </span>
-      </div>
-      <div className='flex items-center justify-start gap-4 w-[650px]'>
-        <div className='relative h-[50px] w-[325px] overflow-hidden text-lg checkout-input-div'>
-          <input
-            type='text'
-            name='creditCardExpiry'
-            autoComplete='off'
-            value={formState.creditCardExpiry.value}
-            onChange={(e) => handleCreditCardExpiry(e.target.value)}
-            onFocus={() => setCardExpiryFocus(true)}
-            onBlur={() => setCardExpiryFocus(false)}
-            maxLength={5}
-            aria-invalid={formState.creditCardExpiry.hasError}
-            aria-describedby='creditcard-expiry-note'
-            aria-label='credit card expiry'
-            className='peer w-full h-full pt-5 text-[#333] outline-none'
-            required
-          />
-          <label
-            className='absolute bottom-0 left-[0%] w-full h-full pointer-events-none border-b border-solid border-black after:content-[" "] after:absolute after:h-full after:w-full after:left-0 -after:bottom-[1px] after:border-b-[2px] after:border-solid after:border-black after:-translate-x-[105%] after:transition-all after:duration-300 after:ease peer-focus:after:translate-x-[0%] peer-valid:after:translate-x-[0%]'
-            htmlFor='creditCardExpiry'
-          >
-            <span
-              className={`absolute bottom-1 left-0 transition-all ease duration-500 ${
-                formState.creditCardExpiry.value.length || cardExpiryFocus
-                  ? "-translate-y-[130%] text-sm"
-                  : " "
-              }`}
+      <div className='flex flex-col sm:flex-row justify-start items-start gap-4 md:w-[650px]'>
+        <div className='md:w-[325px]'>
+          <div className='relative h-[50px] overflow-hidden text-lg checkout-input-div'>
+            <input
+              type='text'
+              name='creditCardExpiry'
+              autoComplete='off'
+              value={formState.creditCardExpiry.value}
+              onChange={(e) => handleCreditCardExpiry(e.target.value)}
+              onFocus={() => setCardExpiryFocus(true)}
+              onBlur={() => setCardExpiryFocus(false)}
+              maxLength={5}
+              aria-invalid={formState.creditCardExpiry.hasError}
+              aria-describedby='creditcard-expiry-note'
+              aria-label='credit card expiry'
+              className='peer w-full h-full pt-5 text-[#333] outline-none'
+              required
+            />
+            <label
+              className='absolute bottom-0 left-[0%] w-full h-full pointer-events-none border-b border-solid border-black after:content-[" "] after:absolute after:h-full after:w-full after:left-0 -after:bottom-[1px] after:border-b-[2px] after:border-solid after:border-black after:-translate-x-[105%] after:transition-all after:duration-300 after:ease peer-focus:after:translate-x-[0%] peer-valid:after:translate-x-[0%]'
+              htmlFor='creditCardExpiry'
             >
-              Valid Thru MM/YY*
-            </span>
-          </label>
-        </div>
-        <div className='relative h-[50px] w-[325px] overflow-hidden text-lg checkout-input-div'>
-          <input
-            type='text'
-            name='creditCardCVC'
-            autoComplete='off'
-            value={formState.creditCardCVC.value}
-            onChange={(e) => handleCreditCardCVC(e.target.value)}
-            onFocus={() => setCardCVCFocus(true)}
-            onBlur={() => setCardCVCFocus(false)}
-            maxLength={4}
-            aria-invalid={formState.creditCardCVC.hasError}
-            aria-describedby='creditcard-cvc-note'
-            aria-label='credit card cvc'
-            className='peer w-full h-full pt-5 text-[#333] outline-none'
-            required
-          />
-          <label
-            className='absolute bottom-0 left-[0%] w-full h-full pointer-events-none border-b border-solid border-black after:content-[" "] after:absolute after:h-full after:w-full after:left-0 -after:bottom-[1px] after:border-b-[2px] after:border-solid after:border-black after:-translate-x-[105%] after:transition-all after:duration-300 after:ease peer-focus:after:translate-x-[0%] peer-valid:after:translate-x-[0%]'
-            htmlFor='creditCardCVC'
-          >
-            <span
-              className={`absolute bottom-1 left-0 transition-all ease duration-500 ${
-                formState.creditCardCVC.value.length || cardCVCFocus
-                  ? "-translate-y-[130%] text-sm"
-                  : " "
-              }`}
-            >
-              CVC*
-            </span>
-          </label>
-        </div>
-      </div>
-      <div className='error-msg-line'>
-        <div className='error-msg'>
+              <span
+                className={`absolute bottom-1 left-0 transition-all ease duration-500 ${
+                  formState.creditCardExpiry.value.length || cardExpiryFocus
+                    ? "-translate-y-[130%] text-sm"
+                    : " "
+                }`}
+              >
+                Valid Thru MM/YY*
+              </span>
+            </label>
+          </div>
           <span
             id='creditcard-expiry-note'
             className={
               formState.creditCardExpiry.hasError
-                ? "error-msg"
-                : "error-msg offscreen"
+                ? "text-rose-600 font-semibold text-lg"
+                : "absolute -left-[9999px]"
             }
             ref={expiryErrorRef}
           >
             {formState.creditCardExpiry.error}
           </span>
         </div>
-        <span
-          id='creditcard-cvc-note'
-          className={
-            formState.creditCardCVC.hasError
-              ? "error-msg"
-              : "error-msg offscreen"
-          }
-          ref={cvcErrorRef}
-        >
-          {formState.creditCardCVC.error}
-        </span>
+        <div className='md:w-[325px]'>
+          <div className='relative h-[50px] overflow-hidden text-lg checkout-input-div'>
+            <input
+              type='text'
+              name='creditCardCVC'
+              autoComplete='off'
+              value={formState.creditCardCVC.value}
+              onChange={(e) => handleCreditCardCVC(e.target.value)}
+              onFocus={() => setCardCVCFocus(true)}
+              onBlur={() => setCardCVCFocus(false)}
+              maxLength={4}
+              aria-invalid={formState.creditCardCVC.hasError}
+              aria-describedby='creditcard-cvc-note'
+              aria-label='credit card cvc'
+              className='peer w-full h-full pt-5 text-[#333] outline-none'
+              required
+            />
+            <label
+              className='absolute bottom-0 left-[0%] w-full h-full pointer-events-none border-b border-solid border-black after:content-[" "] after:absolute after:h-full after:w-full after:left-0 -after:bottom-[1px] after:border-b-[2px] after:border-solid after:border-black after:-translate-x-[105%] after:transition-all after:duration-300 after:ease peer-focus:after:translate-x-[0%] peer-valid:after:translate-x-[0%]'
+              htmlFor='creditCardCVC'
+            >
+              <span
+                className={`absolute bottom-1 left-0 transition-all ease duration-500 ${
+                  formState.creditCardCVC.value.length || cardCVCFocus
+                    ? "-translate-y-[130%] text-sm"
+                    : " "
+                }`}
+              >
+                CVC*
+              </span>
+            </label>
+          </div>
+          <span
+            id='creditcard-cvc-note'
+            className={
+              formState.creditCardCVC.hasError
+                ? "text-rose-600 font-semibold text-lg"
+                : "absolute -left-[9999px]"
+            }
+            ref={cvcErrorRef}
+          >
+            {formState.creditCardCVC.error}
+          </span>
+        </div>
       </div>
     </div>
   );
