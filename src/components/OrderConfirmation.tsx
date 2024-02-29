0a911ca22fe1220883c09e398f121c2e57eaf9e7
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-29 17:03:35 +0900

    Add text center

--- src/components/OrderConfirmation.tsx
+++ src/components/OrderConfirmation.tsx
@@ -5,9 +5,9 @@ const OrderConfirmation = () => {
 
   return (
     <section className='mt-24 px-8 w-full flex flex-col items-center gap-3'>
-      <div className='flex flex-col w-full items-center text-center'>
-        <h2 className='mt-12 text-4xl'>Success!</h2>
-        <p className='text-2xl'>Your order has been recieved.</p>
+      <div className='flex flex-col w-full items-center'>
+        <h2 className='mt-12 text-4xl text-center'>Success!</h2>
+        <p className='text-2xl text-center'>Your order has been recieved.</p>
       </div>
       <p className='text-lg'>Order No: {location?.state?.orderNumber}</p>
       <Link className='text-lg underline' to='/'>
